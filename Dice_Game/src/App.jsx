b892e79ecdf18aa25e863e2aed2414cfcb39b574
rachit <curiousrachit.26@gmail.com>
2023-07-26 23:12:51 +0530

    dice game

---
+++ Dice_Game/src/App.jsx
@@ -0,0 +1,14 @@
+
+import styled from 'styled-components';
+const Button =styled.button`
+background-color:red;
+
+`;
+export default function App() {
+  return (
+  <>
+  <Button>sdfsdf</Button>
+  
+  </>
+  )
+}
